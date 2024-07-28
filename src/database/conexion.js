e7bf9ad0aaad4195d8954ddb4418403bedfff8c4
Svargasowz <sergio.vargasramirez.es@gmail.com>
2024-07-28 18:43:36 -0500

    nueva conexion a otra base de la nube

--- src/database/conexion.js
+++ src/database/conexion.js
@@ -2,9 +2,9 @@ import { createPool } from "mysql2/promise";
 
 
 export const pool = createPool({
-    host:'mysql.railway.internal',
-    user:'root',
-    password:'PxStHYmbDXPNiYrLyeXtiVjzGGCyrxFM',
-    port:'3306',
-    database: 'railway',
+    host:'bjgzprkm0lj9wzuvwfst-mysql.services.clever-cloud.coml',
+    user:'u9valdyliud6ru3c',
+    password:'L5AQDri7BqCn7m4O0qvT',
+    port:3306,
+    database: 'bjgzprkm0lj9wzuvwfst',
 })
