55c4991b2e757192682569676e0f91f91767f600
Svargasowz <sergio.vargasramirez.es@gmail.com>
2024-07-28 18:46:10 -0500

    correcion

--- src/database/conexion.js
+++ src/database/conexion.js
@@ -2,7 +2,7 @@ import { createPool } from "mysql2/promise";
 
 
 export const pool = createPool({
-    host:'bjgzprkm0lj9wzuvwfst-mysql.services.clever-cloud.coml',
+    host:'bjgzprkm0lj9wzuvwfst-mysql.services.clever-cloud.com',
     user:'u9valdyliud6ru3c',
     password:'L5AQDri7BqCn7m4O0qvT',
     port:3306,
